d6390ea624f420164668c1a96534ef82874f9845
Pablo Chico de Guzman <pchico83@gmail.com>
2020-10-16 23:45:20 +0200

    Remove copyright footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,6 @@ import Link from '@material-ui/core/Link';
 function Footer() {
     return (
       <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright © '}
         <Link color="inherit" href="https://material-ui.com/">
           Flower Predictor
         </Link>{' '}
