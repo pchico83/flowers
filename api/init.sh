0ed562bfbfabca350e61d03b47864e8435352ef6
Pablo Chico de Guzman <pchico83@gmail.com>
2020-10-18 20:25:51 +0200

    Add messages

--- api/init.sh
+++ api/init.sh
@@ -9,8 +9,11 @@ then
     echo "PostgreSQL started"
 fi
 
+echo "A"
 python manage.py collectstatic --noinput
+echo "B"
 python manage.py migrate --noinput
+echo "C"
 echo "from django.contrib.auth.models import User;
 User.objects.filter(email='$DJANGO_ADMIN_EMAIL').delete();
 User.objects.create_superuser('$DJANGO_ADMIN_USER', '$DJANGO_ADMIN_EMAIL', '$DJANGO_ADMIN_PASSWORD')" | python manage.py shell
