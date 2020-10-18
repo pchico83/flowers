5d8286a3d9155ca5b683d126c2e329019eddfd76
Pablo Chico de Guzman <pchico83@gmail.com>
2020-10-18 20:31:03 +0200

    Wait for it script

--- api/init-db.sh
+++ api/init-db.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
-if [ "$DATABASE" = "postgres" ]
-then
-    while ! nc -z $DB_HOST $DB_PORT; do
-      echo "Waiting for postgres..."
-      sleep 0.1
-    done
-    
-    echo "PostgreSQL started"
-fi
+echo "Waiting for postgres..."
+./wait-for-it.sh ${DB_HOST}:${DB_PORT}
+echo "PostgreSQL started"
 
-echo "A"
 python manage.py collectstatic --noinput
-echo "B"
 python manage.py migrate --noinput
-echo "C"
 echo "from django.contrib.auth.models import User;
 User.objects.filter(email='$DJANGO_ADMIN_EMAIL').delete();
 User.objects.create_superuser('$DJANGO_ADMIN_USER', '$DJANGO_ADMIN_EMAIL', '$DJANGO_ADMIN_PASSWORD')" | python manage.py shell
